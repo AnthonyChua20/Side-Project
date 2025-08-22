2aa318062653ab8bcfcd9d9a8c4e50c999d0dea0
AnthonyChua20 <87288431+AnthonyChua20@users.noreply.github.com>
2025-08-22 15:07:54 +0800

    Delete Practical2.py

--- Practical2.py
+++
@@ -1,7 +0,0 @@
-GrossIncome = float(input("Enter your gross income: "))
-monthlyGrossincome = GrossIncome / 12
-cpf = monthlyGrossincome * 0.2
-loanPayment = 1500
-MonthlyIncome = monthlyGrossincome - cpf - loanPayment
-print("Your monthly Income after CPF deduction is:", MonthlyIncome)
-
